42f8f5576f9d2aac34fdff110f0fa30f58a3ec0a
gaurabMagar <gdikshan10@gmail.com>
2025-03-13 22:08:33 +0100

    made showroom application inheritance and polimorphism

---
+++ Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+interface utility{
+    public void get_details();
+    public void set_details();
+}
+public class Main {
+    static void main_menu(){
+        System.out.println();
+        System.out.println("***** Welcome to showroom management system *****");
+        System.out.println();
+        System.out.println("**** Enter Your Choice *****");
+        System.out.println();
+        System.out.println("1).ADD Showrooms \t\t\t 2). ADD Employees \t\t\t 3). ADD Cars");
+        System.out.println();
+        System.out.println("4).Get Showrooms \t\t\t 5). Get Employees \t\t\t 6). Get Cars");
+        System.out.println();
+        System.out.println("******** Enter 0 to exit to get out of the application  ********");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Showroom showroom[] = new Showroom[5];
+        Employees employee[] = new Employees[5];
+        Cars car[] = new Cars[5];
+        int car_counter = 0;
+        int showroom_counter = 0;
+        int employees_counter = 0;
+        int choice = 100;
+
+        while(choice!=0){
+            main_menu();
+            choice = sc.nextInt();
+
+            while (choice!=9 && choice!=0){
+                switch(choice){
+                    case 1:
+                        showroom[showroom_counter] = new Showroom();
+                        showroom[showroom_counter].set_details();
+                        showroom_counter++;
+                        System.out.println();
+                        System.out.println("1). ADD new showroom");
+                        System.out.println("9). Go Back main menu");
+                        choice = sc.nextInt();
+                        break;
+                    case 2:
+                        employee[employees_counter] = new Employees();
+                        employee[employees_counter].set_details();
+                        employees_counter++;
+                        System.out.println();
+                        System.out.println("2). ADD new Employee");
+                        System.out.println("9). Go Back main menu");
+                        choice = sc.nextInt();
+                        break;
+                    case 3:
+                        employee[employees_counter] = new Employees();
+                        employee[employees_counter].set_details();
+                        employees_counter++;
+                        System.out.println();
+                        System.out.println("2). ADD new Employee");
+                        System.out.println("9). Go Back main menu");
+                        choice = sc.nextInt();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Showroom.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Showroom implements utility{
+    String showroom_name;
+    String showroom_address;
+    int total_employees;
+    int total_cars_in_stock = 0;
+    String manager_name;
+
+    @Override
+    public void get_details(){
+        System.out.println("Showroom Name" + showroom_name);
+        System.out.println("Showroom Address" + showroom_address);
+        System.out.println("Total Employees" + total_employees);
+        System.out.println("Total Cars In Stock" + total_cars_in_stock);
+    }
+    @Override
+    public void set_details(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("***** ***/ Enter Showroom Details /*** *****");
+        System.out.println();
+        System.out.println("SHOWROOM NAME : ");
+        showroom_name = sc.nextLine();
+        System.out.println();
+        System.out.println("SHOWROOM ADDRESS : ");
+        showroom_address = sc.nextLine();
+        System.out.println();
+        System.out.println("Total Employees");
+        total_employees = sc.nextInt();
+        System.out.println();
+        System.out.println("Total Cars In Stocks");
+        total_cars_in_stock = sc.nextInt();
+    }
+}
